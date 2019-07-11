65aea82961e0edeba361e9c2f5c68de6292d5f23
sumufeng0209 <1796382617@qq.com>
2019-07-11 23:42:44 +0800

    eurekaCommit

---
+++ src/main/java/org/java/InsuranceEurekaApplication.java
@@ -0,0 +1,13 @@
+package org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceEurekaApplication.class, args);
+    }
+
+}
